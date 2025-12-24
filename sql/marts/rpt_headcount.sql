1a46457ace417ac6151441614ddac1c542cd22e2
Fatima Farman <YOUR_GITHUB_EMAIL_HERE>
2025-12-24 06:39:04 +0500

    Update rpt_headcount mart

--- sql/marts/rpt_headcount.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE TABLE `core-rhythm-462516-n5.people_analytics.rpt_headcount` AS
-SELECT
-  CURRENT_DATE() AS as_of_date,
-  COUNTIF(termination_date IS NULL) AS active_headcount,
-  COUNT(*) AS total_employees
-FROM `core-rhythm-462516-n5.people_analytics.dim_employee`;
